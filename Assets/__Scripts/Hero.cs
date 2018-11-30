47848bed979b0c018e3c1cf9e0618a4d8bca26d8
David Farrell <gorillion@users.noreply.github.com>
2018-11-30 02:05:33 -0500

    shields

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour {
 
+    static public Hero S;
+
+    [Header("Set in Inspector")]
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+
+    void Awake()
+    {
+        if (S == null)
+            S = this;
+        else
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+    }
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +28,14 @@ public class Hero : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis *speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
 	}
 }
