9d1fc435426a64b9a4095d105d9d513c73f41dad
David Farrell <gorillion@users.noreply.github.com>
2018-12-14 02:17:47 -0500

    multiple weapons + powerup eating

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -13,6 +13,7 @@ public class Hero : MonoBehaviour {
     public float gameRestartDelay = 2f;
     public GameObject projectilePrefab;
     public float projectileSpeed = 40;
+    public Weapon[] weapons;
 
     [Header("Set Dynamically")]
     [SerializeField]
@@ -88,7 +89,25 @@ public class Hero : MonoBehaviour {
         PowerUp pu = go.GetComponent<PowerUp>();
         switch (pu.type)
         {
+            case WeaponType.shield:
+                shieldLevel++;
+                break;
 
+            default:
+                if(pu.type == weapons[0].type)
+                {
+                    Weapon w = GetEmptyWeaponSlot();
+                    if (w !=null)
+                    {
+                        w.SetType(pu.type);
+                    }
+                    else
+                    {
+                        ClearWeapons();
+                        weapons[0].SetType(pu.type);
+                    }                  
+                }
+                break;
         }
         pu.AbsorbedBy(this.gameObject);
     }
@@ -107,6 +126,27 @@ public class Hero : MonoBehaviour {
                 Destroy(this.gameObject);
                 Main.S.DelayedRestart(gameRestartDelay);
             }
+        }
+    }
+
+    Weapon GetEmptyWeaponSlot()
+    {
+        for (int i=0; i<weapons.Length; i++)
+        {
+            if(weapons[i].type == WeaponType.none)
+            {
+                return (weapons[i]);
+            }
+            
+        }
+        return (null);
+    }
+
+    void ClearWeapons()
+    {
+        foreach (Weapon w in weapons)
+        {
+            w.SetType(WeaponType.none);
         }
     }
 }
